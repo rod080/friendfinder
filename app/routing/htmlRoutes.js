4737d7834a2281c9957c34044e1f181900748606
rod080 <49286063+rod080@users.noreply.github.com>
2019-07-31 22:02:50 -0700

    stufffff

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,25 @@
+
+
+var path = require("path");
+
+//waht does path do again?
+
+//here we are exporting the htmlRoutes
+  module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+      });
+    
+      app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
+  };
+  
+
+
+
+  
